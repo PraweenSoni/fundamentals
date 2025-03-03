ddc3dd7036f88cd74f24abbe041238ed0dd3e32a
Praween Soni <pramrajkumar1234@gmail.com>
2025-03-03 10:53:38 +0530

    arithmetic operation in ruby

---
+++ Ruby/02_arithmetic_operations.rb
@@ -0,0 +1,26 @@
+# Math: Arithetic Operators
+=begin
+    + addition
+    - subtraction
+    * multiplication
+    / devision
+    % module (remainder)
+    ** exponential (Power)
+=end
+
+a = 7;
+b = 2;
+
+puts a + b
+puts a - b
+puts a * b
+puts a % b
+# output : 1
+puts a / b
+# output : 3
+puts a ** b
+# output : 49
+
+c = 3.14  # decimal output because c datatype is decimal
+puts c / b
+# output : 1.57
