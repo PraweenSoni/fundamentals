aa2a9cbfe264eea6835a1e5298f8207adedf3a0a
Praween Soni <pramrajkumar1234@gmail.com>
2025-03-07 17:04:37 +0530

    Java link add repo

--- Java/Basics/helloWorld.java
+++ Java/Basics/helloWorld.java
@@ -1,5 +1,8 @@
 public class helloWorld {
     public static void main(String[] args) {
-        System.out.println("Namaste, World!");
+        System.out.println("Namaste, Java World!");
     }
 }
+
+
+// Here you find all basics of Java :- https://github.com/praweensoni01/Java-Learnings
